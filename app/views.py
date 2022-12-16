26f5605484c3bbf749d693148bc6dbf3aafa3ab4
Pr4vD4 <113354532+Pr4vD4@users.noreply.github.com>
2022-12-16 09:38:38 +0300

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,42 @@
+from app import app
+from flask import render_template, flash ,redirect
+
+from .modules.bg_counter import *
+from .modules.forms import LoginForm
+
+from config import *
+
+
+@app.route('/')
+@app.route('/#')
+def index():
+    data = {
+        'title': 'Game Title',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda cum cumque dolor explicabo hic id illum incidunt, minima possimus quidem quo repellendus sunt tempora veritatis.',
+        'image': 'https://thelastgame.ru/wp-content/uploads/2017/10/header-92-520x245.jpg'
+    }
+    return render_template('pages/index.html', title=WEBSITE_NAME, bg=bg_counter(), page='home', data=data)
+
+
+@app.route('/game/<id>')
+def game(id):
+    return render_template('pages/game.html', title=WEBSITE_NAME, bg=bg_counter(), page='game', id=id)
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    return render_template('pages/register.html', title=WEBSITE_NAME, bg=bg_counter(), page='login')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested Username="' + form.input_username.data + '", Password=' + str(form.input_password.data))
+        return redirect('/')
+    return render_template('pages/login.html', title=WEBSITE_NAME, bg=bg_counter(), page='login', form=form)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('pages/error_page.html', error=error, bg=bg_counter())
